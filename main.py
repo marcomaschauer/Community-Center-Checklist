4cc73b29c03c15acdadb901b53113fd836c8702a
Marco Maschauer <marcomaschauer@outlook.de>
2023-05-26 10:48:44 +0200

    Add Filter + No Refresh
    - Add a filter to change viewed items
    - JS file sends Post requests to python backend now so no refresh nedded anymore
    - Optimize update database values (now only the changed value will be updatet and not everything)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, make_response
-import sqlite3
+import sqlite3, json
  
 # FLASK SETTINGS + INIT
 HOST_NAME = "localhost"
@@ -8,10 +8,19 @@ DBFILE = "./data/community_center_data.db"
 app = Flask(__name__)
 
 # GET ALL ITEMS FROM DATABASE 
-def getitems():
+def getitems(filter = "all"):
   conn = sqlite3.connect(DBFILE)
   cursor = conn.cursor()
-  cursor.execute("SELECT * FROM `community_center`")
+  match filter:
+    case 'all': cursor.execute("SELECT * FROM `community_center`"),
+    case 'checked': cursor.execute("SELECT * FROM `community_center` WHERE checked = 'true' "),
+    case 'unchecked': cursor.execute("SELECT * FROM `community_center` WHERE checked = 'false' "),
+    case 'spring': cursor.execute("SELECT * FROM `community_center` WHERE category = 'spring' "),
+    case 'summer': cursor.execute("SELECT * FROM `community_center` WHERE category = 'summer' "),
+    case 'fall': cursor.execute("SELECT * FROM `community_center` WHERE category = 'fall' "),
+    case 'winter': cursor.execute("SELECT * FROM `community_center` WHERE category = 'winter' "),
+    case 'other': cursor.execute("SELECT * FROM `community_center` WHERE category = 'other' "),
+    case other: cursor.execute("SELECT * FROM `community_center`")
   results = cursor.fetchall()
   conn.close()
   return results
@@ -22,19 +31,24 @@ def index():
   if request.method == 'GET':
     items = getitems()
     return render_template("table.html", item = items)
-  else: #POST JS Script das on klick event abfängt und dann den Post request an das Python Backend weitergibt mit Element welches sich geändert hat un den Zustand. Also "Apfel" und "checked". 
-    data = request.form.getlist('checkbox')
+  else: #POST
+    if request.form.get('filter') != None:
+      items = getitems(request.form.get('filter'))
+      return render_template("table.html", item = items)
+    print(request.form.get('filter'))
+    data = request.json
     conn = sqlite3.connect(DBFILE)
     cursor = conn.cursor()
-    cursor.execute(f"UPDATE `community_center` SET checked = 'false' WHERE checked = 'true'")
-    conn.commit()
-    for item in data:
-      cursor.execute(f"UPDATE `community_center` SET checked = 'true' WHERE name = '{item}'")
+    item_name = data["item_name"]
+    if data["item_checked"]:
+      cursor.execute(f"UPDATE `community_center` SET checked = 'true' WHERE name = '{item_name}'")
+      conn.commit()
+    else:
+      cursor.execute(f"UPDATE `community_center` SET checked = 'false' WHERE name = '{item_name}'")
       conn.commit()
     conn.close()
-    items = getitems()
-    return render_template("table.html", item = items)
- 
+    return 'Received !' # response to your request.
+
 # START
 if __name__ == "__main__":
   app.run(HOST_NAME, HOST_PORT)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,17 @@
-function submit() {
-    document.getElementById("item_list").submit();
-}
+addEventListener("change", (event) => {});
+
+onchange = (event) => {
+    fetch("http://127.0.0.1:5000/", {
+        method: "POST",
+        body: JSON.stringify({
+            //filter: document.getElementById("filter-select").value,
+            item_name: event.target.id,
+            item_checked: event.target.checked
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+
+};
